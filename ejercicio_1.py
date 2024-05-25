de0b6bff846bbc1c4313c6ebc6925ffd7b447c79
DiegoGomez <diegocor4@gmail.com>
2024-05-25 00:00:54 -0300

    ejercicio 1 completo

---
+++ ejercicio_1.py
@@ -0,0 +1,11 @@
+"""
+1. Crear un programa que almacene en una lista las materias de esta u otra carrera y que las
+muestre por pantalla. (La lista debe ser predefinida, no debe ser ingresada por el usuario).
+"""
+
+materias = ['Introduccion a la informatica', 'Programacion 1', 'Bases de Datos', 'Programacion 2', 'Diseño Grafico']
+
+print('Lista de materias de la carrera: ')
+for materia in materias: 
+    print(materia)
+
