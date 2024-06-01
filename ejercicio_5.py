a906a6acd8c2e02668c8eb50967f88c4bdbec55d
Diego Gomez <diegocor4@gmail.com>
2024-06-01 11:03:13 -0300

    5completo

--- ejercicio_5.py
+++ ejercicio_5.py
@@ -16,31 +16,39 @@ def ingresar_numeros():
 
     while True:
         entrada = input('Ingrese un numero entero o "fin" para ternminar: ')
-        if entrada.lower() == 'fin':
+
+        if not entrada:  # Si la entrada esta vacia
+            if not numeros:  # Y la lista de números también está vacía
+                print("No se ingresaron números.")
+                break  # Salir del bucle
+            else:
+                continue  # Continuar pidiendo entrada
+
+        if entrada.lower() == 'fin': # si ingresa fin, inicia las operaciones sobre la lista
             print('fin de la carga')
             break
         
         if entrada.isdigit() or entrada[0] == '-' and entrada[1:].isdigit(): # si es nunero entero positivo o negativo
             numero = int(entrada)
             numeros.append(numero)
-        else:
-            print('la entrada no es valida por favor ingrese un nujmero entero. ')
 
-        print(numeros) 
-    
+        else:
+            print('la entrada no es valida por favor ingrese un numero entero. ')
+
+        print(numeros)
+  
     return numeros  
 
 
-
-def maximo_en_lista(list):
-    maximo = max(list)
+def maximo_en_lista(lis):
+    maximo = max(lis)
     print("El maximo de la lista es: ", maximo)
 
 
 
-def segundo_maximo(list):
+def segundo_maximo(lis):
     #convierte en conjunto para eliminar elementos duplicados, y los ordena de manera descendente
-    numeros_ordenados = sorted(set(list), reverse=True)
+    numeros_ordenados = sorted(set(lis), reverse=True)
 
     #si la longitud de la lista es mayor que uno toma el segundo elemento sino, asigna none a la variable segundo maximo
     segundo_maximo = numeros_ordenados[1] if len(numeros_ordenados) > 1 else None
@@ -48,20 +56,39 @@ def segundo_maximo(list):
 
 
 
-def minimo_en_lista(list):
-    minimo = min(list)
+def minimo_en_lista(lis):
+    minimo = min(lis)
     print("El mínimo es:", minimo)
 
 
 
-def multiplicar_elementos(list):
+def multiplicar_elementos(lis):
     multiplicacion = 1
-    for elemento in list:
+    for elemento in lis:
         multiplicacion *= elemento
     print("La multiplicación de todos los números es:", multiplicacion)
 
 
+def contar_pares_impares(lis):
+    pares = 0
+    for elemento in lis:
+        if elemento % 2 == 0:
+            pares += 1
 
+    impares = len(lis) - pares
+
+    print("Cantidad de números pares:", pares)
+    print("Cantidad de números impares:", impares)
+
+
+def remover_repetidos(lis):
+    elementos_sin_repetir = list(set(lis))
+    print("Lista sin elementos repetidos:", elementos_sin_repetir)
+
+
+
+
+# Ingresan los numeros
 lista = ingresar_numeros()
 
 # a. Determinar el maximo
@@ -75,6 +102,12 @@ minimo_en_lista(lista)
 
 #d. Calcular la multiplicación de  todos los números de la lista.
 multiplicar_elementos(lista)
+
+#e. Contar los valores pares e impares.
+contar_pares_impares(lista)
+
+#f. Remover los elementos repetidos
+remover_repetidos(lista)
 
 
 
