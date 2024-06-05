8366bda81c5d999a2f792527a18f233e2b3779f0
DiegoGomez <diegocor4@gmail.com>
2024-06-05 20:57:56 -0300

    ejercicio6 completado

--- ejercicio_6.py
+++ ejercicio_6.py
@@ -9,6 +9,43 @@ d. Quitar un elemento al principio.
 
 
 
+# a, agregar un elemento al final.
+def lista_agregar_final(elem,lis):
+    lis.append(elem)
+    print('Elemento agregado al final de la lista')
+    print(lis)
+
+
+#b. agregar un elememento al principio
+def lista_agregar_principio(elem,lis):
+    lis.insert(0,elem)
+    print('Elemento agregado al principio de la lista')
+    print(lis)
+
+
+#c. quitar un elemento al final
+def lista_quitar_final(lis):
+    if lis:
+        elemento = lista.pop()
+        print(f'El elemento {elemento} fue removido del final de la lista ')
+        print(lis)
+    else:
+        print('La lista esta vacia')
+
+
+#d. Quitar un elemento del principio
+def lista_quitar_principio(lis):
+    if lis:
+        elemento = lis.pop(0)
+        print(f'El elemento {elemento} fue removido del principio de la lista')
+        print(lis)
+    else:
+        print('La lista esta vacia.')
+
+def lista_mostrar(lis):
+    print('Contenido de la lista', lis)
+
+lista = []
 
 while True:
     print('¿Que accion desea realizar?')
@@ -17,22 +54,22 @@ while True:
     print('c. Quitar un elemento al final')
     print('d. Quitar un elemento al principio')
     print('e. Mostrar la lista')
-    print('q. Sallir')
+    print('q. Salir')
 
     opcion = input('Ingrese una opcion: ')
 
     if opcion == 'a':
         elemento = input('Ingrese el elemento que desea agregar al final: ')
-        lista_agregar_final(elemento)
+        lista_agregar_final(elemento,lista)
     elif opcion == 'b':
         elemento = input('Ingrese el elemento que desea agregar al principio: ')
-        lista_agregar_principio(elemento)
+        lista_agregar_principio(elemento,lista)
     elif opcion == 'c':
-        lista_quitar_final()
+        lista_quitar_final(lista)
     elif opcion == 'd':
-        lista_quitar_principio()
+        lista_quitar_principio(lista)
     elif opcion == 'e':
-        lista_mostrar()
+        lista_mostrar(lista)
     elif opcion == 'q': 
         print('saliendo')
         break
