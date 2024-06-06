5d996956ea6d6338d5149e426c169f2bec49c89d
DiegoGomez <diegocor4@gmail.com>
2024-06-05 21:29:59 -0300

    ejercicio7 en proceso

---
+++ ejercicio_7.py
@@ -0,0 +1,36 @@
+"""
+7. Escriba un programa Python que cuente con dos listas, la primera de ellas almacenará strings
+con tareas pendientes y la segunda almacenará strings con tareas terminadas. Permita al
+usuario:
+a. Agregar nuevas tareas pendientes.
+b. Marcar las tareas pendientes como terminadas. Al hacer esto, la tarea deberá pasar
+de la lista de pendientes a la de terminadas.
+Nota: posterior a cada operación deberá mostrar por pantalla el estado actual de ambas
+listas.
+"""
+
+
+def agregar_tarea_pendiente():
+    pass
+
+
+def marcar_tarea_como_terminada(tareas_pendientes, tareas_terminadas):
+    pass
+
+
+def mostrar_listas(tareas_pendientes, tareas_terminadas)
+    pass
+
+
+lista_pendientes = []
+lista_terminadas = []
+
+while True:
+    print('Elija una opcion:')
+    print('a. Agregar una nueva tarea pendiente')
+    print('b. Marcar una tarea pendiente como terminada')
+    print('c. Mostrar las listas de tareas')
+    print('q. Salir')
+
+    
+
